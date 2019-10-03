7799dae904631f6bd36eb1755b174bdb102f4519
Artemiy <fiskonerone@gmail.com>
2019-10-03 11:51:27 +0300

    testing started

---
+++ TestProj/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TestProj")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4fd42f87-4f8e-416b-913c-06a26b370c72")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestProj/RegTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommandProject;
+using System.ServiceModel;
+
+namespace TestProj
+{
+    [TestClass]
+    public class RegTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Server.ServerService obj = new Server.ServerService();
+            obj.Registration()
+        }
+    }
+}
